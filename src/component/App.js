155c31110243300840c7fcfab608e8b45b9137e7
Haishion <haishion@xmu.edu.cn>
2021-02-23 10:33:04 +0800

    add github pages

--- src/component/App.js
+++ src/component/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Row, Col } from 'antd';
+import React from "react";
+import { Row, Col } from "antd";
 
-import CustomData from '../logic/CustomData';
-import DueDateTable from './DueDateTable';
-import FilteredCategoryRules from './FilteredCategoryRules';
-import Memo from './Memo';
-import KaoHeCalendar from './KaoHeCalendar';
+import CustomData from "../logic/CustomData";
+import DueDateTable from "./DueDateTable";
+import FilteredCategoryRules from "./FilteredCategoryRules";
+import Memo from "./Memo";
+import KaoHeCalendar from "./KaoHeCalendar";
 
-import './App.css';
-
+import "./App.css";
 
 export default class App extends React.Component {
-
   initKaoHe() {
     this.kh = CustomData.initKaoHe();
   }
 
   getFilteredCategory(filterCallback) {
-    let customDateRulesCategories = []
+    let customDateRulesCategories = [];
     for (let c of this.kh.categories) {
-      let rs = []
+      let rs = [];
       for (let r of c.rules) {
         if (filterCallback(r)) {
           rs.push(r);
@@ -39,20 +37,24 @@ export default class App extends React.Component {
 
     this.initKaoHe();
 
-    let calendarEvents = []
-    this.kh.getAllDueDate().forEach(rule => {
+    let calendarEvents = [];
+    this.kh.getAllDueDate().forEach((rule) => {
       calendarEvents.push({
         name: rule.title,
         startDate: rule.dueDate,
-        endDate: rule.dueDate
+        endDate: rule.dueDate,
       });
-    })
+    });
 
     this.state = {
       calendarEvents: calendarEvents,
-      customDateRulesCategories: this.getFilteredCategory(rule => rule.isCustomDate),
-      submitWhenChangeCategories: this.getFilteredCategory(rule => rule.isSubmitWhenChange),
-      irregularCategories: this.getFilteredCategory(rule => rule.isIrregular),
+      customDateRulesCategories: this.getFilteredCategory(
+        (rule) => rule.isCustomDate
+      ),
+      submitWhenChangeCategories: this.getFilteredCategory(
+        (rule) => rule.isSubmitWhenChange
+      ),
+      irregularCategories: this.getFilteredCategory((rule) => rule.isIrregular),
       categories: this.kh.categories,
       allDueDateRules: this.kh.getAllDueDateSorted(),
     };
@@ -72,11 +74,20 @@ export default class App extends React.Component {
                 <Memo />
               </Col>
               <Col span={5}>
-                <FilteredCategoryRules title='个性化时间点' categories={this.state.customDateRulesCategories} />
-                <FilteredCategoryRules title='有变化才提交' categories={this.state.submitWhenChangeCategories} />
+                <FilteredCategoryRules
+                  title="个性化时间点"
+                  categories={this.state.customDateRulesCategories}
+                />
+                <FilteredCategoryRules
+                  title="有变化才提交"
+                  categories={this.state.submitWhenChangeCategories}
+                />
               </Col>
               <Col span={7}>
-                <FilteredCategoryRules title='长期坚持' categories={this.state.irregularCategories} />
+                <FilteredCategoryRules
+                  title="长期坚持"
+                  categories={this.state.irregularCategories}
+                />
               </Col>
             </Row>
           </Col>
@@ -88,11 +99,13 @@ export default class App extends React.Component {
         </Row>
         <Row>
           <Col span={6}>
-            <FilteredCategoryRules title='全部' categories={this.state.categories} />
+            <FilteredCategoryRules
+              title="全部"
+              categories={this.state.categories}
+            />
           </Col>
         </Row>
       </>
     );
   }
 }
-
